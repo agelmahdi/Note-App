ee1cd946aeb1911175429c0b5f17dfe109a598ac
Ahmed El-Mahdi <agelmahdi@gmail.com>
2021-10-31 17:01:39 +0200

    [Integration test] test interaction between notesscreen and viewModel

---
+++ app/src/androidTest/java/com/agelmahdi/noteapp/di/AppModuleTest.kt
@@ -0,0 +1,44 @@
+package com.agelmahdi.noteapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.agelmahdi.noteapp.feature_note.data.data_source.NoteDatabase
+import com.agelmahdi.noteapp.feature_note.data.repository.NoteRepositoryImpl
+import com.agelmahdi.noteapp.feature_note.domain.repository.NoteRepository
+import com.agelmahdi.noteapp.feature_note.domain.use_case.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModuleTest {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.inMemoryDatabaseBuilder(
+            app,
+            NoteDatabase::class.java
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository{
+        return NoteRepositoryImpl(db.noteDAO)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUseCases(noteRepository: NoteRepository): NoteUseCases{
+        return NoteUseCases(
+            getNotes = GetNotesUC(noteRepository),
+            deleteNote = DeleteNoteUC(noteRepository),
+            addNote = AddNoteUC(noteRepository),
+            getNote = GetNoteUC(noteRepository)
+        )
+    }
+}
---
+++ app/src/main/java/com/agelmahdi/noteapp/core/util/TestTags.kt
@@ -0,0 +1,5 @@
+package com.agelmahdi.noteapp.core.util
+
+object TestTags {
+    const val ORDER_SECTION= "ORDER_SECTION"
+}
